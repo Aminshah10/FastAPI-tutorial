83ea693dac53aea4604f244c8930cca750214f83
AminShah10 <aminshahmohamadi1384@gmail.com>
2026-07-21 20:25:47 +0330

    post and retrieve by id added

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
-
 
 @app.get("/")
 def hello_world():
     return {"message": "Hello World"}
-
 
 names_db = [
     {"id": 1, "name": "ali"},
@@ -14,7 +13,19 @@ names_db = [
     {"id": 3, "name": "arousha"},
 ]
 
-
 @app.get("/names")
 def names_list():
     return names_db
+
+@app.post("/names")
+def create_name(name:str):
+    new_obj = {"id": random.randint(4, 100), "name": name}
+    names_db.append(new_obj)
+    return names_db
+
+@app.get("/names/{name_id}")
+def retrive_name_detail(name_id:int):
+    for names in names_db:
+        if names["id"] ==  name_id:
+            return names
+    return {"detail": "object not found"}
