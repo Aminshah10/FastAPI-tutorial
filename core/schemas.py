132f403c16671576eca7ab69fa13f6922e8c6896
AminShah10 <aminshahmohamadi1384@gmail.com>
2026-08-09 18:26:50 +0330

    pydantic field_validator added.

--- core/schemas.py
+++ core/schemas.py
@@ -1,7 +1,15 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 class PersonBaseSchema(BaseModel):
     name : str
+    
+    @field_validator("name")
+    def validate_name(cls, value):
+        if len(value) > 32:
+            raise ValueError("name must not exceed 32 characters")
+        if not value.isalpha():
+            raise ValueError("Name must contain only alphabetic characters")
+        return value
     
 class PersonCreateSchema(PersonBaseSchema):
     pass
