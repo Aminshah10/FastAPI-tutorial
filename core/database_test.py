264149b4110b9bae462c75a73341626076d7c298
AminShah10 <aminshahmohamadi1384@gmail.com>
2026-08-10 23:18:46 +0330

    creating a session and adding users

--- core/database_test.py
+++ core/database_test.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./docs/databse.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./docs/database.db"
 # SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 # for postgres or other relational databases
@@ -19,6 +19,71 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 # create base class for declaring tables
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(30))
+    last_name = Column(String(30), nullable=True)
+    age = Column(Integer)
+    
+    @property
+    def get_fullname(self):
+        return f"{self.first_name} {self.last_name or ''}".strip()
+    
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, fullname={self.get_fullname})"
 
 # to create tables and database
-Base.metadata.create_all(engine)
+Base.metadata.create_all(bind=engine)
+
+# Create one database session.
+session = SessionLocal()
+
+
+try:
+    # --------------------------------
+    # Add one user
+    # --------------------------------
+
+    user = User(
+        first_name="Ali Bigdeli",
+        age=30
+    )
+
+    # Add the user to the current transaction.
+    session.add(user)
+
+    # Save the transaction to the database.
+    session.commit()
+
+
+    # --------------------------------
+    # Add multiple users
+    # --------------------------------
+
+    users = [
+        User(first_name="Maryam", age=26),
+        User(first_name="Arousha", age=6)
+    ]
+
+    # Add multiple objects at once.
+    session.add_all(users)
+
+    # Save them to the database.
+    session.commit()
+
+
+    # --------------------------------
+    # Read users
+    # --------------------------------
+
+    all_users = session.query(User).all()
+
+    for user in all_users:
+        print(user)
+
+
+finally:
+    # Always close the session when finished.
+    session.close()
